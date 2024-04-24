b2eb6af17f1cd3a2277a0fb500b8679e258061b5
Saheb Sutradhar <sahebsutradhar92@gmail.com>
2024-04-24 13:42:25 +0530

    Project init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className=' bg-gray-100'>
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react'
+import logo from '../logo.png';
+
+// icons
+import { MdMenuOpen } from "react-icons/md";
+import { IoHomeOutline } from "react-icons/io5";
+import { FaProductHunt } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { TbReportSearch } from "react-icons/tb";
+import { IoLogoBuffer } from "react-icons/io";
+import { CiSettings } from "react-icons/ci";
+import { MdOutlineDashboard } from "react-icons/md";
+
+const menuItems = [
+  {
+    icons: <IoHomeOutline size={30} />,
+    label: 'Home'
+  },
+  {
+    icons: <FaProductHunt size={30} />,
+    label: 'Products'
+  },
+  {
+    icons: <MdOutlineDashboard size={30} />,
+    label: 'Dashboard'
+  },
+  {
+    icons: <CiSettings size={30} />,
+    label: 'Setting'
+  },
+  {
+    icons: <IoLogoBuffer size={30} />,
+    label: 'Log'
+  },
+  {
+    icons: <TbReportSearch size={30} />,
+    label: 'Report'
+  }
+]
+
+export default function Sidebar() {
+
+  const [open, setOpen] = useState(true)
+
+  return (
+    <nav className={`shadow-md h-screen p-2 flex flex-col duration-500 bg-blue-600 text-white ${open ? 'w-60' : 'w-16'}`}>
+
+      {/* Header */}
+      <div className=' px-3 py-2 h-20 flex justify-between items-center'>
+        <img src={logo} alt="Logo" className={`${open ? 'w-10' : 'w-0'} rounded-md`} />
+        <div><MdMenuOpen size={34} className={` duration-500 cursor-pointer ${!open && ' rotate-180'}`} onClick={() => setOpen(!open)} /></div>
+      </div>
+
+      {/* Body */}
+
+      <ul className='flex-1'>
+        {
+          menuItems.map((item, index) => {
+            return (
+              <li key={index} className='px-3 py-2 my-2 hover:bg-blue-800 rounded-md duration-300 cursor-pointer flex gap-2 items-center relative group'>
+                <div>{item.icons}</div>
+                <p className={`${!open && 'w-0 translate-x-24'} duration-500 overflow-hidden`}>{item.label}</p>
+                <p className={`${open && 'hidden'} absolute left-32 shadow-md rounded-md
+                 w-0 p-0 text-black bg-white duration-100 overflow-hidden group-hover:w-fit group-hover:p-2 group-hover:left-16
+                `}>{item.label}</p>
+              </li>
+            )
+          })
+        }
+      </ul>
+      {/* footer */}
+      <div className='flex items-center gap-2 px-3 py-2'>
+        <div><FaUserCircle size={30} /></div>
+        <div className={`leading-5 ${!open && 'w-0 translate-x-24'} duration-500 overflow-hidden`}>
+          <p>Saheb</p>
+          <span className='text-xs'>saheb@gmail.com</span>
+
+        </div>
+      </div>
+
+
+    </nav>
+  )
+}
